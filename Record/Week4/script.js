d25ed58e0ad95c4d8adbcf36edb621de01f635af
BhanuPrasad-270106 <amarabhanuprasad.24.csm@anits.edu.in>
2026-08-10 23:47:06 +0530

    Update script.js

--- Record/Week4/script.js
+++ Record/Week4/script.js
@@ -1,1 +1,46 @@
 
+let nameInput = document.getElementById("studentName");
+let ageInput = document.getElementById("studentAge");
+let courseInput = document.getElementById("studentCourse");
+
+let addButton = document.getElementById("addStudent");
+
+let studentTable = document.getElementById("studentTable");
+let message = document.getElementById("message");
+
+
+addButton.addEventListener("click", function() {
+
+    let name = nameInput.value;
+    let age = ageInput.value;
+    let course = courseInput.value;
+
+
+    if (name === "" || age === "" || course === "") {
+
+        message.textContent = "Please fill all fields";
+
+        return;
+    }
+
+
+    let row = document.createElement("tr");
+
+    let nameCell = document.createElement("td");
+    let ageCell = document.createElement("td");
+    let courseCell = document.createElement("td");
+
+
+    nameCell.textContent = name;
+    ageCell.textContent = age;
+    courseCell.textContent = course;
+
+
+    row.appendChild(nameCell);
+    row.appendChild(ageCell);
+    row.appendChild(courseCell);
+
+
+    studentTable.appendChild(row);
+
+});
