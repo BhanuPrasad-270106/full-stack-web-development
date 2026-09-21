a8464010ab9ff652b1612bd15067a93e7e38965d
BhanuPrasad-270106 <amarabhanuprasad.24.csm@anits.edu.in>
2026-09-21 21:38:40 +0530

    Update fileManagement.js

--- Observation Tasks/Task5/FileManagement/fileManagement.js
+++ Observation Tasks/Task5/FileManagement/fileManagement.js
@@ -1,1 +1,47 @@
+const fs = require("fs");
+const readline = require("readline");
 
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Enter file name: ", (filename) => {
+    rl.question("Enter content: ", (content) => {
+
+        // Create and write content to the file
+        fs.writeFile(filename, content, (err) => {
+            if (err) throw err;
+
+            console.log("\nFile created and content written successfully.");
+
+            // Read the file
+            fs.readFile(filename, "utf8", (err, data) => {
+                if (err) throw err;
+
+                console.log("\nInitial file content:");
+                console.log(data);
+
+                rl.question("\nEnter additional content: ", (additionalContent) => {
+
+                    // Append additional content
+                    fs.appendFile(filename, "\n" + additionalContent, (err) => {
+                        if (err) throw err;
+
+                        console.log("\nContent appended successfully.");
+
+                        // Read and display final content
+                        fs.readFile(filename, "utf8", (err, finalData) => {
+                            if (err) throw err;
+
+                            console.log("\nFinal file content:");
+                            console.log(finalData);
+
+                            rl.close();
+                        });
+                    });
+                });
+            });
+        });
+    });
+});
